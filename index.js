463371efb7a37c752fe04a13463f555851f69283
tv786 <tahavarda52@gmail.com>
2024-04-22 09:35:23 +0530

    reorder

--- index.js
+++ index.js
@@ -52,26 +52,25 @@ function addProject(projectName, imageUrl, siteUrl) {
 }
 
 // Add Projects
+addProject('Angel Wealth Advisor', './images/8.jpeg', 'https://angelwealthadvisors.com/');
+addProject('Hidden Smiles', './images/3.jpeg', 'http://hidden-smiles.com/');
+addProject('LEPI Venture', './images/9.jpeg', '#');
+addProject('Tech Now', './images/21.jpeg', 'https://tech-now.io/');
+addProject('Eurodoors', './images/15.jpeg', 'https://eurodoors.org/');
+addProject('Siberia Snus', './images/23.jpeg', 'https://www.siberia-snus.de/');
+addProject('Venture Capital', './images/22.jpeg', '#');
+addProject('Gutachter24', './images/16.jpeg', 'https://gutachter24.berlin/');
 addProject('Go-Finance', './images/1.jpeg', 'https://go-infofinance.com/');
 addProject('Go-tech Solutions', './images/2.jpeg', 'https://go-techsolution.org/');
-addProject('Hidden Smiles', './images/3.jpeg', 'http://hidden-smiles.com/');
 addProject('Jawahar Deep cleaning', './images/4.jpeg', 'https://jawahardeepcleaning.com/');
 addProject('Travinix', './images/5.jpeg', 'https://visittravenex.com/');
 addProject('Turquoise', './images/6.jpeg', 'https://trqauto.com/');
 addProject('Tiru Pooja Store', './images/7.jpeg', 'https://tirupoojastore.in/');
-addProject('Angel Wealth Advisor', './images/8.jpeg', 'https://angelwealthadvisors.com/');
-addProject('LEPI Venture', './images/9.jpeg', '#');
-addProject('Tech Now', './images/10.jpeg', 'https://tech-now.io/');
 addProject('Weddings For U', './images/11.jpeg', 'https://weddingsforu.in/');
 addProject('Aryavart', './images/12.jpeg', 'https://aryavarttls.org/');
 addProject('YK Infra Trading', './images/13.jpeg', 'https://ykinfratrading.com/');
 addProject('Raj Astrologer', './images/14.jpeg', 'https://rajastrologer.in/');
-addProject('Eurodoors', './images/15.jpeg', 'https://eurodoors.org/');
-addProject('Gutachter24', './images/16.jpeg', 'https://gutachter24.berlin/');
 addProject('PDS Clicks', './images/17.jpeg', 'https://pdsclicks.in/');
 addProject('Alaikha', './images/18.jpeg', 'https://alaikha.com/');
 addProject('4D', './images/19.jpeg', 'https://www.thefourd.com/');
 addProject('Swarit Studios', './images/20.jpeg', '#');
-addProject('Tech Now', './images/21.jpeg', 'https://tech-now.io/');
-addProject('Venture Capital', './images/22.jpeg', '#');
-addProject('Siberia Snus', './images/23.jpeg', 'https://www.siberia-snus.de/');
